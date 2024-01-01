2da88c39c4e7f37d479da6ea2e2b5a1a0d91e5d2
Joy Sarkar <joysarkar8171@gmail.com>
2024-01-01 22:52:29 +0530

    gradient added

--- lib/glossy.dart
+++ lib/glossy.dart
@@ -15,6 +15,7 @@ class GlossyContainer extends StatelessWidget {
   final double? strengthX;
   final double? strengthY;
   final BoxBorder? border;
+  final BlendMode? blendMode;
   final GlossyGradient? gradient;
 
   const GlossyContainer({
@@ -28,12 +29,19 @@ class GlossyContainer extends StatelessWidget {
     this.borderWith,
     this.border,
     this.color,
+    this.blendMode,
     this.gradient,
     required this.child,
   });
 
   @override
   Widget build(BuildContext context) {
+    late LinearGradient lg;
+    if (gradient is GlossyGradient) {
+      final GlossyLinearGradient glg = gradient as GlossyLinearGradient;
+      lg = glg.getLinearGradient(opacity);
+    }
+
     return ClipRect(
       child: Container(
         height: height,
@@ -41,6 +49,7 @@ class GlossyContainer extends StatelessWidget {
         color: Colors.transparent,
         child: Stack(children: [
           BackdropFilter(
+            blendMode: blendMode != null ? blendMode! : BlendMode.srcOver,
             filter: ImageFilter.blur(
               sigmaX: strengthX == null ? 30 : strengthX!,
               sigmaY: strengthY == null ? 30 : strengthY!,
@@ -51,22 +60,25 @@ class GlossyContainer extends StatelessWidget {
             height: height,
             width: width,
             decoration: BoxDecoration(
-                borderRadius: borderRadius,
-                border: border ??
-                    Border.all(
-                      color: Colors.grey,
-                      width: 0.5,
-                    ),
-                gradient: LinearGradient(
-                    tileMode: TileMode.mirror,
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      // Color.fromARGB(255, 107, 106, 106).withOpacity(0.153),
-                      // Color.fromARGB(255, 88, 88, 88).withOpacity(0.103),
-                      color!.withOpacity(opacity),
-                      Colors.red.withOpacity(opacity)
-                    ])),
+              borderRadius: borderRadius,
+              border: border ??
+                  Border.all(
+                    color: Colors.grey,
+                    width: 0.5,
+                  ),
+              gradient: gradient == null
+                  ? LinearGradient(
+                      tileMode: TileMode.mirror,
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                          // Color.fromARGB(255, 107, 106, 106).withOpacity(0.153),
+                          // Color.fromARGB(255, 88, 88, 88).withOpacity(0.103),
+                          color!.withOpacity(opacity),
+                          Colors.red.withOpacity(opacity)
+                        ])
+                  : lg,
+            ),
           ),
           Container(
             child: child,
@@ -93,4 +105,12 @@ class GlossyLinearGradient extends GlossyGradient {
   final AlignmentGeometry begin;
   final AlignmentGeometry end;
   final TileMode tileMode;
+
+  LinearGradient getLinearGradient(double opacity) {
+    return LinearGradient(
+        colors: colors.map((e) => e.withOpacity(opacity)).toList(),
+        begin: begin,
+        end: end,
+        tileMode: tileMode);
+  }
 }
